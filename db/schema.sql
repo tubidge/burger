95a00055205eb115d65e55513dfa264ec0dafae2
tubidge <tubidge@gmail.com>
2019-05-11 16:38:28 -0600

    fixed schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,6 +3,7 @@ USE burgers_db;
 
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT,
-    burger_name VARCHAR NOT NULL,
-    devoured BOOLEAN NOT NULL
+    burger_name VARCHAR(100) NOT NULL,
+    devoured BOOLEAN NOT NULL,
+    primary key (id)
 )
