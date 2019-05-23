01af75516fea184154770ad7639787accdc0153a
tubidge <tubidge@gmail.com>
2019-05-22 18:14:09 -0600

    fixed some jquery methods

--- models/burger.js
+++ models/burger.js
@@ -5,8 +5,9 @@ var burger = $(function () {
     $('#create-form').on('submit', function (event) {
         event.preventDefault();
 
-        $.ajax('/', {
-            type: 'POST',
+        $.ajax({
+            url: '/',
+            method: 'POST',
             data: $('#burger-name').val().trim()
         }).then(function () {
             console.log('Added new burger!');
@@ -14,15 +15,16 @@ var burger = $(function () {
         });
     });
 
-    // $('#devour-btn').on('click', function (event) {
-    //     $.ajax('/', {
-    //         type: 'PUT',
-    //         data: $(this).data('id')
-    //     }).then(function () {
-    //         console.log('You devoured that burger!');
-    //         location.reload();
-    //     });
-    // });
+    $('#devour-btn').on('click', function (event) {
+        $.ajax({
+            url: '/',
+            method: 'PUT',
+            data: $(this).data('id')
+        }).then(function () {
+            console.log('You devoured that burger!');
+            location.reload();
+        });
+    });
 });
 
 // Exporting 'burger' for use in the controller.js file.
